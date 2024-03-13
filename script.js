023e407142998adf390e7543256a7a8da1aeba37
hanabiwork21 <115154453+hanabiwork21@users.noreply.github.com>
2024-03-13 21:40:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Toggle Icon
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+// Navbar sticky
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100)
+
 // Service section pop up
 function showPopup(id) {
     var popup = document.getElementById("popupContainer" + id);
